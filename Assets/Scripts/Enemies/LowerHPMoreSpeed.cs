0c36ffd64868af4d07636de6f073817ba7bd6249
Dario-ESAT <artilesdi@esat-alumni.com>
2021-11-25 23:58:59 +0100

    bye bye laser, frist enemies scripts

---
+++ Assets/Scripts/Enemies/LowerHPMoreSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowerHPMoreSpeed : EntityReaction{
+
+  LinearMovement movement_;
+  void Start() {
+      
+  }
+  protected override void OnHit(Health health, EventArgs eventArgs) {
+
+    // c_health;
+  }
+
+  protected override void OnDie(Health health, EventArgs eventArgs) {
+      
+      Destroy(gameObject);
+  }
+}
---
+++ Assets/Scripts/Enemies/TeleportAtHit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportAtHit : EntityReaction{
+protected override void OnHit(Health health, EventArgs eventArgs) {
+
+}
+
+protected override void OnDie(Health health, EventArgs eventArgs) {
+		
+		Destroy(gameObject);
+	}
+}
